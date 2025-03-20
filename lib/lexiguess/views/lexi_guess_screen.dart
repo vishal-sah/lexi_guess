8bbc69e0b09b55bd6eb32c1a131dc415a56f9844
vishal-sah <vs6812048@gmail.com>
2025-03-20 23:35:48 +0530

    Add game logic and UI for Lexi Guess game, including word list

--- lib/lexiguess/views/lexi_guess_screen.dart
+++ lib/lexiguess/views/lexi_guess_screen.dart
@@ -1,15 +1,58 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+import 'package:flutter/material.dart';
+import 'package:lexi_guess/lexiguess/data/word_list.dart';
+import 'package:lexi_guess/lexiguess/models/letter_model.dart';
+import 'package:lexi_guess/lexiguess/models/word_model.dart';
+
+enum GameStatus { playing, submitting, won, lost }
+
+class LexiGuessScreen extends StatefulWidget {
+  const LexiGuessScreen({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<LexiGuessScreen> createState() => _MyWidgetState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _MyWidgetState extends State<LexiGuessScreen> {
+  GameStatus gameStatus = GameStatus.playing;
+
+  final List<Word> _board = List.generate(
+    6,
+    (_) => Word(letters: List.generate(5, (_) => Letter.empty())),
+  );
+
+  int _currWordIndex = 0;
+
+  Word? get _currWord =>
+      _currWordIndex < _board.length ? _board[_currWordIndex] : null;
+
+  Word _solution = Word.fromString(
+    fiveLetterWords[Random().nextInt(fiveLetterWords.length)].toUpperCase(),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          'LEXI GUESS',
+          style: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 4,
+          ),
+        )
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Board(board: _board),
+        ],
+      ),
+    );
   }
 }
